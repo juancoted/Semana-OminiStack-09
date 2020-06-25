7c26c7bfbed0bc947c172260c07b61bb98f83532
Juan Costa <juandiegocds@gmail.com>
2020-06-25 10:53:57 -0300

    mobile
    Aplicativo testado com o Expo

---
+++ mobile/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Routes from './src/routes';
+//view tem a mesma função da DIV
+//Toda tag de texto é um TEXT
+
+export default function App() {
+  return <Routes />
+}
